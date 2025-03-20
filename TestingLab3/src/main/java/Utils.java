120cd0cfd97c09b7260718e41ac64be7bc0f0693
ASUS <hokure.04@gmail.com>
2025-03-20 05:25:29 +0300

    tests complited

---
+++ TestingLab3/src/main/java/Utils.java
@@ -0,0 +1,53 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Utils {
+    public WebDriver driver;
+    public JavascriptExecutor js;
+    public Map<String, Object> vars;
+    public WebDriverWait wait;
+
+    public void setupDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+        driver.manage().window().setSize(new Dimension(1050, 716));
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<>();
+        driver.get("https://otvet.mail.ru/");
+    }
+
+    public WebDriver getDriver(){
+        return driver;
+    }
+
+    public WebDriverWait getWaitTime(){
+        return wait;
+    }
+
+    public JavascriptExecutor getJsExecutor(){
+        return js;
+    }
+
+    public Map<String, Object> getVars(){
+        return vars;
+    }
+
+    public void quitDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+
+}
